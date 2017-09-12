a188c7697e455c4c8480e700b76d9149f33ba7ca
alexandru.rolea <alexandru.rolea@bearingpoint.com>
2017-09-12 12:39:16 +0300

    REmoved unused import

--- wikia-frontend/app/navigator/navigator.module.ts
+++ wikia-frontend/app/navigator/navigator.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
-import {PostsComponent} from './posts.component';
 import {NavigatorComponent} from './navigator.component';
 
 @NgModule({
